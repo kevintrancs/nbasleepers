345b90f3340f5ff919e7c89abe4a50a8e26641a2
Kevin Tran <kevintrancs@gmail.com>
2019-04-20 17:26:45 -0700

    split training and tables

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import csv
+import random
+import numpy as np
 from csv import DictWriter, DictReader
 headers = ['player_name', 'college', 'draft_year', 'draft_round', 'draft_number',
            'gp', 'pts', 'reb', 'ast', 'net_rating', 'usg_pct', 'ts_pct', 'ast_pct']
@@ -52,5 +54,48 @@ def generate_career_avgs(player):
     player.ast_pct = sum(player.ast_pct) / len(player.ast_pct)
 
 
+def sort_to_table(players):
+    table = []
+    for k, v in players.items():
+        table.append([v.player_name, v.draft_number, v.gp, v.pts, v.reb,
+                      v.ast, v.net_rating, v.usg_pct, v.ts_pct, v.ast_pct])
+    return table
+
+# 0 = name, 1 = draftNumber (classifier)
+
+
+def get_column(table, index):
+    col = []
+    for row in table:
+        col.append([[row[0]], row[index]])
+    return col
+
+
+def compute_holdout_partitions(table):
+
+    randomized = table[:]
+    n = len(randomized)
+    for i in range(n):
+        rand_index = random.randrange(0, n)
+        randomized[i], randomized[rand_index] = randomized[rand_index], randomized[i]
+    split_index = int(2 / 3 * n)
+    train_set = randomized[:split_index]
+    test_set = randomized[split_index:]
+    return train_set, test_set
+
+
+def calc_slope(X, Y, mean_x, mean_y):
+    numer = 0
+    denom = 0
+    m = len(X)
+
+    for i in range(m):
+        numer += (X[i] - mean_x) * (Y[i] - mean_y)
+        denom += (X[i] - mean_x) ** 2
+    slope = numer / denom
+    return slope
+
+
 if __name__ == '__main__':
     players = org_players('all_seasons.csv', headers)
+    table = sort_to_table(players)
